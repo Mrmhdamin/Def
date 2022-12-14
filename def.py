5f3381742db8193cf42dda60f70744696c723e6a
MohamadAmin <aminnoorinajafi@gmail.com>
2022-12-14 15:58:15 +0330

    Create def.py

---
+++ def.py
@@ -0,0 +1,18 @@
+# def hello(name,age):
+#     print("hello " + name + " your age " + age)
+#     print("Good lock")
+
+# first_name = input("Plese enter name : ")
+# age = input("Please enter age : ")
+# hello(first_name , age )
+
+
+def add_number(num1,num2):
+    sum = num1 + num2
+    return sum
+
+num1 =int(input("please enter first number : "))
+num2 =int(input("please enter secend number : "))
+
+result = add_number(num1,num2)
+print(result)
